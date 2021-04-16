d9571985a22926d74c07986ff298ea098ba84e6e
fdchevalier <fcheval@txbiomed.org>
2021-04-15 20:00:43 -0500

    Update the R script for CNV analysis

--- scripts/PZQ-R_GWAS_CNV.R
+++ scripts/PZQ-R_GWAS_CNV.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 # Title: PZQ-R_GWAS_CNV.R
-# Version: 0.1
+# Version: 0.2
 # Author: Frédéric CHEVALIER <fcheval@txbiomed.org>
 # Created in: 2018-03-12
-# Modified in: 2019-10-17
+# Modified in: 2021-04-15
 
 
 
@@ -11,6 +11,7 @@
 # Comments #
 #==========#
 
+# v0.2 - 2021-04-15: adapt to the notebook environment
 # v0.1 - 2019-10-17: adapt script to the whole genome sequencing data
 # v0.0 - 2018-03-12: creation
 
@@ -19,22 +20,45 @@
 #==========#
 # Pacakges #
 #==========#
+cat("Loading packages...\n")
 
-library(cn.mops)
-library(Biobase)
-library(parallel)
+suppressMessages({
+    library(cn.mops)
+    library(Biobase)
+    library(parallel)
+})
+
+
+
+#===========#
+# Variables #
+#===========#
+
+cat("Setting variables...\n")
+
+# Working directory
+setwd(file.path(getwd(), "scripts"))
+
+# Folders
+data_fd   <- "../data/"
+graph_fd  <- "../graphs/"
+result_fd <- "../results/1-QTL/"
+
 
 exp.tag <- c("Exp1","Exp2")
 
-BAMFolder <- "../1-Alignment/data/" 
+BAMFolder <- paste0(data_fd, "libraries/1-GWAS/")
 
-if(! dir.exists("graphs/")) { dir.create("graphs/") }
+if(! dir.exists(graph_fd)) { dir.create(graph_fd) }
 
-#segments  <- read.table("~/data/sm_exons/sma_agilent_baits.v7.0.chr_reorderd.bed", sep="\t", as.is=TRUE)
+mychr <- paste0("SM_V7_", c(1:7, "ZW"))
 
-mychr <- c(paste0("Chr_",seq(1,7)), "Chr_W")
-
-n.cores <- detectCores()
+# Number of core used
+if (detectCores() > 120) { 
+    n.cores <- 120
+} else {
+    n.cores <- detectCores()
+}
 
 # To store call table
 call.tb.ls <- vector("list", length(exp.tag))
@@ -43,13 +67,13 @@ names(call.tb.ls) <- exp.tag
 for (t in exp.tag) {
     
     # List of BAM files
-    BAMFiles <- list.files(BAMFolder, pattern=paste0(t,".*bam$"), recursive=TRUE, full.name=TRUE)
+    BAMFiles <- list.files(BAMFolder, pattern = paste0(t,".*bam$"), recursive = TRUE, full.name = TRUE)
 
     # Get read counts for the group
-    X  <- getReadCountsFromBAM(BAMFiles, refSeqNames=mychr, parallel=n.cores)
+    X  <- getReadCountsFromBAM(BAMFiles, refSeqNames = mychr, parallel = n.cores)
 
     # Scan for CNVs
-    resCNMOPS <- referencecn.mops(X[,4:6], X[,1:3], minWidth=3, segAlgorithm="fast", parallel=n.cores)
+    resCNMOPS <- referencecn.mops(X[,4:6], X[,1:3], minWidth = 3, segAlgorithm = "fast", parallel = n.cores)
 
     # Number of regions with CNVs
     nb.regions <- length(resCNMOPS@cnvr)
@@ -63,8 +87,8 @@ for (t in exp.tag) {
         regions <- as.vector(resCNMOPS@cnvr@seqnames)
 
         for (i in 1:nb.regions) {
-            pdf(paste0("graphs/CNV_region_",i,"_",regions[i],"_",t,".pdf"))
-                plot(resCNMOPS, which=i, toFile=TRUE)
+            pdf(paste0(graph_fd, "CNV_region_", i, "_", regions[i], "_", t, ".pdf"))
+                plot(resCNMOPS, which = i, toFile = TRUE)
             dev.off()
         }
 
@@ -75,14 +99,14 @@ for (t in exp.tag) {
 
             for (i in 4:ncol(call.tb.chr)) {
                 call.val <- call.tb.chr[,c(2:3,i)]
-                myname   <- paste(strsplit(colnames(call.tb.chr)[i], "_")[[1]][1:4], collapse=" ")
+                myname   <- paste(strsplit(colnames(call.tb.chr)[i], "_")[[1]][1:4], collapse = " ")
 
-                pdf(paste0("graphs/CNV_",c,"_",myname,".pdf"), width=13, height=8)
+                pdf(paste0(graph_fd, "CNV_", c, "_", myname, ".pdf"), width = 13, height = 8)
                     my.xlim <- max(call.val[,2])
                     my.ylim <- max(abs(call.val[,3]))
                     if (my.ylim < 1) { my.ylim <- 1 }
-                    plot(0,xlim=c(1,my.xlim), ylim=c(-my.ylim, my.ylim), type="n", xlab="Position (bp)", ylab="Log ratio of the call", main=myname)
-                    segments(call.val[,1], call.val[,3], call.val[,2], lwd=5, col="red")
+                    plot(0,xlim = c(1,my.xlim), ylim = c(-my.ylim, my.ylim), type = "n", xlab = "Position (bp)", ylab = "Log ratio of the call", main = myname)
+                    segments(call.val[,1], call.val[,3], call.val[,2], lwd = 5, col = "red")
                 dev.off()
             }
         }
