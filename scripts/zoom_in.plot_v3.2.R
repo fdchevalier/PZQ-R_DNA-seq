060683fd22f1c8846785109269ec8465623351ca
fdchevalier <fcheval@txbiomed.org>
2021-08-10 21:05:59 -0500

    Clean code of the zoom QTL script

--- scripts/zoom_in.plot_v3.2.R
+++ scripts/zoom_in.plot_v3.2.R
@@ -28,15 +28,15 @@ mychr <- "SM_V7_3"
 exp.tag <- c("Exp1","Exp2")
 
 # Load GFF
-# mygff.fl <- "~/data/sm_Gene_table/Sm_v7.1_renamed.gff"
-mygff.fl <- "~/data/sm_Gene_table/schistosoma_mansoni.PRJEA36577.WBPS14.annotations.gff3"
+mygff.fl <- "../data/genome/schistosoma_mansoni.PRJEA36577.WBPS14.annotations.gff3"
 mygff <- readGFF(mygff.fl)
 mygff.genes <- mygff[ mygff[,3] == "gene", ]
 
 # Gene expression
-# myexpr <- read.csv("~/data/sm_Gene_table/TPM_isoforms_Sm_v7.1.tsv", header=TRUE, sep="\t")
-myexpr <- read.csv("~/analyses/00 - Misc/2018-04-19 v7 genome gene expression/TPM_isoforms_Sm_2020-08-07.tsv", header=TRUE, sep="\t")
+myexpr <- read.csv("../data/genome/Sm_transcript_table_gff-hhpred_2020-09-21.tsv", header=TRUE, sep="\t")
 
+# Gene of interest
+goi <- "Smp_246790"
 
 myseed <- 553309
 
@@ -67,9 +67,6 @@ e <- e[,-1]
 # Reorder data
 e <- e[,c(2:4,1,ncol(e),6)]
 
-#levels(e[,6])[grep("+", levels(e[,6]), fixed=T)] <- "+1"
-#levels(e[,6])[grep("-", levels(e[,6]), fixed=T)] <- "-1"
-
 e[,6] <- as.numeric(e[,6])
 e[e[,6] == 2, 6] <- -1
 e[e[,6] == 3, 6] <- 0
@@ -89,18 +86,10 @@ myp.val.tb  <- cbind(mydata[,1:2], mydata[,myp.val.cln])
 myp.val.tb  <- myp.val.tb[! is.na(myp.val.tb[, 3]), ]
 
 # Bonferroni correction
-# bf.cor <- -log10(0.05/nrow(mydata))
 bf.cor <- 0.05/nrow(myp.val.tb)
 my.qtl <- myp.val.tb[myp.val.tb[,3] <= bf.cor & myp.val.tb[,1] == mychr, 2]
-# bf.lim <- c(my.qtl[1], my.qtl[length(my.qtl)])
 bf.lim <- c(my.qtl[1], my.qtl[which(diff(my.qtl) > 1e6)[1]])
 
-# Sub-sampling
-# set.seed(myseed)
-# myrow <- rownames(myp.val.tb[myp.val.tb[,3] > bf.cor, ]) %>% sample(., dim(myp.val.tb)[1] * 0.1)
-# myrow <- rownames(myp.val.tb[myp.val.tb[,3] <= bf.cor, ]) %>% c(., myrow) %>% as.numeric() %>% sort() %>% as.character()
-
-# myp.val.tb <- myp.val.tb[myrow, ]
 myp.val.tb  <- myp.val.tb[! is.na(myp.val.tb[, 3]), ]
 
 
@@ -112,18 +101,12 @@ myp.val.tb  <- myp.val.tb[! is.na(myp.val.tb[, 3]), ]
 my.x1 <- 1
 my.x2 <- 5e6
 
-my.x1b <- 6.5e5
-my.x2b <- 1.25e6
-#my.x2b <- 7.5e5
-
-# mygff.genes <- mygff.genes[ mygff.genes[,1] == mychr & mygff.genes[,4] >= bf.lim[1] & mygff.genes[,5] <= bf.lim[2] , ]
 mygff.genes <- mygff.genes[ mygff.genes[,1] == mychr & mygff.genes[,4] >= my.x1 & mygff.genes[,5] <= my.x2 , ]
 
 # Gene expression
 mygenes <- mygff.genes[, "Name"]
 mycol   <- NULL
 for (i in mygenes) {
-    # exp.gene <- (myexpr[ myexpr[,1] == i, 11:12] > 0) %>% sum(.) > 0
     exp.gene <- (myexpr[ grepl(paste0(i, "\\."), myexpr[,1]), 14:15] > 0) %>% sum(.) > 0
     if (exp.gene) {
         mycol[i] <- "black"
@@ -138,12 +121,9 @@ myp.val.tb.chr[,3] <- -log10(myp.val.tb.chr[,3])
 
 myp.val.tb <- rename_chr_SmV7(myp.val.tb, 1)
 
-#png(paste0("graphs/zoom_",mychr,"_QTL.png"), width=13*72, height=16*72)
-#pdf(paste0("graphs/zoom_",mychr,"_QTL.pdf"), width=8, height=10, useDingbats=FALSE)
 # pdf(paste0("../graphs/zoom_",mychr,"_QTL_v3.pdf"), width=8, height=10, useDingbats=FALSE)
-png(paste0("../graphs/zoom_",mychr,"_QTL_v3_2.png"), width=8*72, height=10*72)
+png(paste0("../graphs/zoom_",mychr,"_QTL_v3.png"), width=8*72, height=10*72)
     
-#    layout(matrix(1:4, ncol=1), height=c(1,0.7,0.3,1))
     layout(matrix(1:3, ncol=1), height=c(1,0.7,0.3))
     
     par(mar=c(5,5,2,2)+0.1)
@@ -159,7 +139,6 @@ png(paste0("../graphs/zoom_",mychr,"_QTL_v3_2.png"), width=8*72, height=10*72)
 
     zoomsregion(c(my.x1, my.x2) + myshift)
 
-
     # Plot 2: p-values
     par(mar=c(0,5,4,2)+0.1)
 
@@ -170,11 +149,10 @@ png(paste0("../graphs/zoom_",mychr,"_QTL_v3_2.png"), width=8*72, height=10*72)
 
     ablineclip(h=-log10(bf.cor), x1=0, x2=my.x2,, col="blue", lty=3, lwd=2)
 
-#    rect(my.x1b, par("usr")[3]-10, my.x2b, par("usr")[4], lty=2)
     
     ## Annotations
     axis (2, at=format(seq(0,21,length=5),digits=2), cex.axis=mycex.axis)
-    mtext(2, text=expression(-Log[10]*" p-value"), line=3) #, cex=mycex)
+    mtext(2, text=expression(-Log[10] ~ p-value), line=3) #, cex=mycex)
 
     # Plot 3: genes
     par(mar=c(5,5,0,2)+0.1)
@@ -183,34 +161,25 @@ png(paste0("../graphs/zoom_",mychr,"_QTL_v3_2.png"), width=8*72, height=10*72)
 
     for(g in 1:nrow(mygff.genes)) {
         rect(mygff.genes[g,4], -my.y, mygff.genes[g,5], my.y, col=mycol[g], border=NA)
+
+        if (mygff.genes[g, "Name"] == goi) {
+            old_xpd <- par("xpd")
+            par(xpd = NA)
+
+            bx_y <- ((par("usr")[4] - par("usr")[3]) * 0.05) %>% abs()
+            bx_x <- bx_y * 10^floor(log10(diff(par("usr")[1:2])))
+            
+            rect(mygff.genes[g,4] - bx_x, -my.y - bx_y, mygff.genes[g,5] + bx_x, 2.25, lty = 2)
+
+            par(xpd = old_xpd)
+        }
     }
+
+
 
     axis (1, at=seq(par("xaxp")[1], par("xaxp")[2], length=par("xaxp")[3]+1), labels=seq(par("xaxp")[1], par("xaxp")[2], length=par("xaxp")[3]+1)/1e6, cex.axis=mycex.axis)
     mtext(1, text="Position (Mb)", line=3) #, cex=mycex)
 
 
-##    zoomsregion(c(my.x1b, my.x2b), extend=c(10,0))
-#    zoomsregion(c(my.x1b, my.x2b))
-#
-#    # Plot 4: genes (and not isoforms!!)
-#    par(mar=c(5,5,2,2)+0.1)
-#    e.tmp <- e[ e[,1] == mychr & (e[,2] >= my.x1b & e[,3] <= my.x2b) , ]
-#    
-#    ## Add expression data from adult males
-#    for (i in unique(e.tmp[,4])) { 
-#        myexpr.tmp <- max(myexpr[ grepl(i, myexpr[,1]) , 11], na.rm=T)
-#        if (length(myexpr.tmp) > 0) { e.tmp[ e.tmp[,4] == i , 5 ] <- log10(myexpr.tmp) }
-#    }
-#
-#    e.tmp[,5] <- as.numeric(e.tmp[,5])
-#
-#    e.tmp <- e.tmp[order(e.tmp[,2],e.tmp[,4]),]
-#    
-#    pg <- plotGenes(e.tmp, mychr, my.x1b, my.x2b, labeltext=TRUE, colorby=e.tmp[,5], colorbycol=SushiColors(5), colorbyrange=c(0,max(e.tmp[,5])), fontsize=0.5, plotgenetype="arrow", bentline=FALSE, wigglefactor=0.1)
-#    addlegend(pg[[1]], palette=pg[[2]], title=expression(Log[10] * "TPM"), side="left", xoffset=-0.1, bottominset=0.5)
-#    
-#    axis (1, at=seq(par("xaxp")[1], par("xaxp")[2], length=par("xaxp")[3]+1), labels=seq(par("xaxp")[1], par("xaxp")[2], length=par("xaxp")[3]+1)/1e6)
-#    mtext(1, text="Position (Mb)", line=3)
- 
 dev.off()
 
